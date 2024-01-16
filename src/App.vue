dacd5ba16fceb30893b46fbbf9b288f390d86c3a
GuerraSalasIvan <ivan.guerra.salas.al@iespoligonosur.org>
2024-01-16 12:31:18 +0000

    vue

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script>
+
+import Block from './components/Block.vue'
+export default{
+  name:'App',
+  components: { Block },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random()*5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+  </header>
+
+  
+    <h1>Ninja Reaction Timer</h1>
+
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay"/>
+
+
+  
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="block" v-if="showBlock">
+        click me
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['delay'],
+    data(){
+        return {
+            showBlock: false
+        }
+    },
+    mounted(){
+        setTimeout(() => {
+            this.showBlock = true;
+        }, this.delay)
+
+    },
+    updated(){
+        
+    }
+}
+
+</script>
+
+<style>
+.block{
+    width:400px;
+    border-radius: 20px;
+    background-color: coral;
+    color: white;
+    padding: 100px 0;
+    margin: 40px auto;
+    text-align: center;
+}
+</style>
